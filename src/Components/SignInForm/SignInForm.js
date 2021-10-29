5845098706dcb30ea263db610bc98d643d3ffec2
Maksim <easypeasylemonskvii@gmail.com>
2021-10-29 13:47:57 +0300

    TT: refactor signIn form

--- src/Components/SignInForm/SignInForm.js
+++ src/Components/SignInForm/SignInForm.js
@@ -9,27 +9,16 @@ export default class SignInForm extends Component {
 			email: '',
 			password: '',
 		};
-		this.onLoginInputChange = this.onLoginInputChange.bind(this);
-		this.onEmailInputChange = this.onEmailInputChange.bind(this);
-		this.onPasswordInputChange = this.onPasswordInputChange.bind(this);
+		this.onFormInputChange = this.onFormInputChange.bind(this);
 		this.onSubmitSignInForm = this.onSubmitSignInForm.bind(this);
 	}
 
-	onLoginInputChange(event) {
+	onFormInputChange(event) {
+		const evTarget = event.target;
+		const evValue = evTarget.value;
+		const evName = evTarget.name;
 		this.setState({
-			login: event.target.value,
-		});
-	}
-
-	onEmailInputChange(event) {
-		this.setState({
-			email: event.target.value,
-		});
-	}
-
-	onPasswordInputChange(event) {
-		this.setState({
-			password: event.target.value,
+			[evName]: evValue,
 		});
 	}
 
@@ -50,21 +39,21 @@ export default class SignInForm extends Component {
 							type="text"
 							name="login"
 							placeholder="Username"
-							onChange={this.onLoginInputChange}
+							onChange={this.onFormInputChange}
 						/>
 
 						<input
 							type="email"
 							name="email"
 							placeholder="Email"
-							onChange={this.onEmailInputChange}
+							onChange={this.onFormInputChange}
 						/>
 
 						<input
 							type="password"
 							name="password"
 							placeholder="Password"
-							onChange={this.onPasswordInputChange}
+							onChange={this.onFormInputChange}
 						/>
 
 						<input type="submit" value="Sign Up" />
