4fb6f2f28befea90bb9857921f0607e0a992aab4
Maksim <easypeasylemonskvii@gmail.com>
2021-11-05 16:23:20 +0300

    TT: edit redux-thunk

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -7,6 +7,9 @@ import Weather from '../Weather/Weather';
 import LocalStorageServices from '../../Services/LocalStorageServices';
 import { removeSearchHistory } from '../../Actions/RemoveSearchHistory';
 import { getWeatherById, getWeatherByName } from '../../Reducers/Weather';
+import { addNearestWeather } from '../../Actions/NearestWeather';
+import { addSearchHistory } from '../../Actions/SearchHistoryAction';
+import { addCurntWeather } from '../../Actions/CurrentWeatherAction/Index';
 
 class Main extends Component {
 	componentDidUpdate(prevProps) {
@@ -17,137 +20,12 @@ class Main extends Component {
 		}
 	}
 
-	submitRequestByName = (value) => {
-		console.log(this.props);
+	submitRequestByName = async (value) => {
 		this.props.getByName(value, this.props);
-		// await searchByName(value)
-		// 	.then((response) => {
-		// 		const dt = Date(response.data.dt);
-		// 		const crntDate = dt.slice(0, 15);
-		// 		const time = dt.slice(16, 24);
-		// 		const tempArr = response.data.list;
-		//
-		// 		// eslint-disable-next-line no-shadow
-		// 		const { nearestWeather, addCurntWeather, addNearestWeather } =
-		// 			this.props;
-		// 		// eslint-disable-next-line no-plusplus
-		// 		for (let index = 0; index < tempArr.length; index++) {
-		// 			if (index === 0) {
-		// 				const newCurWeather = {
-		// 					name: response.data.city.name,
-		// 					country: response.data.city.country,
-		// 					temp: Math.ceil(tempArr[index].main.temp),
-		// 					feel: Math.floor(tempArr[index].main.feels_like),
-		// 					crntDate,
-		// 					time,
-		// 					weather: tempArr[index].weather,
-		// 				};
-		// 				addCurntWeather(newCurWeather);
-		// 			} else {
-		// 				const newNearestWeather = {
-		// 					temp: Math.ceil(tempArr[index].main.temp),
-		// 					feels_like: Math.floor(tempArr[index].main.feels_like),
-		// 					wind: tempArr[index].wind.speed,
-		// 					time: tempArr[index].dt_txt.slice(11, 20),
-		// 					id: tempArr[index].dt,
-		// 				};
-		//
-		// 				if (nearestWeather.length === 4) {
-		// 					// eslint-disable-next-line no-param-reassign
-		// 					nearestWeather.splice(0, nearestWeather.length);
-		//
-		// 					return newNearestWeather;
-		// 				}
-		// 				addNearestWeather(newNearestWeather);
-		// 			}
-		// 		}
-		//
-		// 		const { searchHistory } = this.props;
-		// 		const newPost = {
-		// 			name: response.data.city.name,
-		// 			country: response.data.city.country,
-		// 			id: response.data.city.id,
-		// 		};
-		// 		const { id } = response.data.city;
-		// 		// eslint-disable-next-line no-shadow
-		// 		const { addSearchHistory } = this.props;
-		// 		const elem = searchHistory.find((item) => item.id === id);
-		// 		if (elem) {
-		// 			const index = searchHistory.indexOf(elem);
-		// 			searchHistory.splice(index, 1);
-		//
-		// 			const newData = [...searchHistory, newPost];
-		// 			addSearchHistory(newPost);
-		//
-		// 			return newData;
-		// 		}
-		// 		const newData = [...searchHistory, newPost];
-		// 		addSearchHistory(newPost);
-		//
-		// 		LocalStorageServices.addSearchHistory(JSON.stringify(newData));
-		// 	})
-		// 	.catch((e) => {
-		// 		alert(wrongValue, e);
-		// 	});
 	};
 
 	submitRequestById = async (id) => {
 		this.props.getById(id, this.props);
-		// const response = await searchById(id);
-		// const dt = Date(response.data.dt);
-		// const crntDate = dt.slice(0, 15);
-		// const time = dt.slice(16, 24);
-		//
-		// const tempArr = response.data.list;
-		// // eslint-disable-next-line no-shadow
-		// const { nearestWeather, addCurntWeather, addNearestWeather } = this.props;
-		// for (let index = 0; index < tempArr.length; index = index + 1) {
-		// 	if (index === 0) {
-		// 		const newCurWeather = {
-		// 			name: response.data.city.name,
-		// 			country: response.data.city.country,
-		// 			temp: Math.ceil(tempArr[index].main.temp),
-		// 			feel: Math.floor(tempArr[index].main.feels_like),
-		// 			crntDate,
-		// 			time,
-		// 			weather: tempArr[index].weather,
-		// 		};
-		// 		addCurntWeather(newCurWeather);
-		// 	} else {
-		// 		const newNearestWeather = {
-		// 			temp: Math.ceil(tempArr[index].main.temp),
-		// 			feels_like: Math.floor(tempArr[index].main.feels_like),
-		// 			wind: tempArr[index].wind.speed,
-		// 			time: tempArr[index].dt_txt.slice(11, 20),
-		// 			id: tempArr[index].dt,
-		// 		};
-		//
-		// 		if (nearestWeather.length === 4) {
-		// 			// eslint-disable-next-line no-param-reassign
-		// 			nearestWeather.splice(0, nearestWeather.length);
-		//
-		// 			return newNearestWeather;
-		// 		}
-		// 		addNearestWeather(newNearestWeather);
-		// 	}
-		// }
-		//
-		// const { searchHistory } = this.props;
-		// // eslint-disable-next-line no-shadow
-		// const { addSearchHistory } = this.props;
-		// const newPost = {
-		// 	name: response.data.city.name,
-		// 	country: response.data.city.country,
-		// 	id: response.data.city.id,
-		// };
-		//
-		// const elem = searchHistory.find((item) => item.id === id);
-		// const index = searchHistory.indexOf(elem);
-		// searchHistory.splice(index, 1);
-		// const newData = [...searchHistory, newPost];
-		// addSearchHistory(newPost);
-		//
-		// LocalStorageServices.addSearchHistory(JSON.stringify(newData));
 	};
 
 	render() {
@@ -185,6 +63,9 @@ export default connect(
 		nearestWeather: state.nearestWeather,
 	}),
 	{
+		addCurntWeather,
+		addNearestWeather,
+		addSearchHistory,
 		removeSearchHistory,
 		getByName: getWeatherByName,
 		getById: getWeatherById,
--- src/Services/SetWeatherServices.js
+++ src/Services/SetWeatherServices.js
@@ -1,76 +1,95 @@
 // import LocalStorageServices from './LocalStorageServices';
-//
-// const setWeatherValue = (response, props, dispatch) => {
-// 	console.log(response);
-// 	console.log(props);
-// 	const dt = Date(response.data.dt);
-// 	const crntDate = dt.slice(0, 15);
-// 	const time = dt.slice(16, 24);
-// 	const tempArr = response.data.list;
-//
-// 	const {
-// 		nearestWeather,
-// 		searchHistory,
-// 		addCurntWeather,
-// 		addNearestWeather,
-// 		addSearchHistory,
-// 	} = props;
-// 	// eslint-disable-next-line no-plusplus
-// 	for (let index = 0; index < tempArr.length; index++) {
-// 		if (index === 0) {
-// 			const newCurWeather = {
-// 				name: response.data.city.name,
-// 				country: response.data.city.country,
-// 				temp: Math.ceil(tempArr[index].main.temp),
-// 				feel: Math.floor(tempArr[index].main.feels_like),
-// 				crntDate,
-// 				time,
-// 				weather: tempArr[index].weather,
-// 			};
-// 			dispatch(addCurntWeather(newCurWeather));
-// 			console.log(newCurWeather);
-// 		} else {
-// 			const newNearestWeather = {
-// 				temp: Math.ceil(tempArr[index].main.temp),
-// 				feels_like: Math.floor(tempArr[index].main.feels_like),
-// 				wind: tempArr[index].wind.speed,
-// 				time: tempArr[index].dt_txt.slice(11, 20),
-// 				id: tempArr[index].dt,
-// 			};
-//
-// 			if (nearestWeather.length === 4) {
-// 				// eslint-disable-next-line no-param-reassign
-// 				nearestWeather.splice(0, nearestWeather.length);
-//
-// 				return newNearestWeather;
-// 			}
-// 			addNearestWeather(newNearestWeather);
-// 		}
-// 	}
-//
-// 	// const { searchHistory } = this.props;
-// 	const newPost = {
-// 		name: response.data.city.name,
-// 		country: response.data.city.country,
-// 		id: response.data.city.id,
-// 	};
-// 	const { id } = response.data.city;
-// 	// eslint-disable-next-line no-shadow
-// 	// const { addSearchHistory } = this.props;
-// 	const elem = searchHistory.find((item) => item.id === id);
-// 	if (elem) {
-// 		const index = searchHistory.indexOf(elem);
-// 		searchHistory.splice(index, 1);
-//
-// 		const newData = [...searchHistory, newPost];
-// 		addSearchHistory(newPost);
-//
-// 		return newData;
-// 	}
-// 	const newData = [...searchHistory, newPost];
-// 	dispatch(addSearchHistory(newPost));
-//
-// 	LocalStorageServices.addSearchHistory(JSON.stringify(newData));
-// };
-//
-// export default setWeatherValue;
+
+import LocalStorageServices from './LocalStorageServices';
+
+const setCurrentWeather = (index, response, arr) => {
+	const dt = Date(response.data.dt);
+	const crntDate = dt.slice(0, 15);
+	const time = dt.slice(16, 24);
+
+	const newCurWeather = {
+		name: response.data.city.name,
+		country: response.data.city.country,
+		temp: Math.ceil(arr[index].main.temp),
+		feel: Math.floor(arr[index].main.feels_like),
+		crntDate,
+		time,
+		weather: arr[index].weather,
+	};
+
+	return newCurWeather;
+};
+
+const setNearestWeather = (index, arr, nearestWeather) => {
+	const newNearestWeather = {
+		temp: Math.ceil(arr[index].main.temp),
+		feels_like: Math.floor(arr[index].main.feels_like),
+		wind: arr[index].wind.speed,
+		time: arr[index].dt_txt.slice(11, 20),
+		id: arr[index].dt,
+	};
+
+	if (nearestWeather.length === 4) {
+		// eslint-disable-next-line no-param-reassign
+		nearestWeather.splice(0, nearestWeather.length);
+
+		return newNearestWeather;
+	}
+
+	return newNearestWeather;
+};
+
+const setSearchHistory = (response, searchHistory, addSearchHistory) => {
+	const newPost = {
+		name: response.data.city.name,
+		country: response.data.city.country,
+		id: response.data.city.id,
+	};
+	const { id } = response.data.city;
+	const elem = searchHistory.find((item) => item.id === id);
+	if (elem) {
+		const index = searchHistory.indexOf(elem);
+		searchHistory.splice(index, 1);
+
+		const newData = [...searchHistory, newPost];
+		addSearchHistory(newPost);
+
+		return newData;
+	}
+	const newData = [...searchHistory, newPost];
+	addSearchHistory(newPost);
+
+	LocalStorageServices.addSearchHistory(JSON.stringify(newData));
+};
+
+const setWeatherValue = (response, props) => {
+	const tempArr = response.data.list;
+	const {
+		nearestWeather,
+		searchHistory,
+		addCurntWeather,
+		addNearestWeather,
+		addSearchHistory,
+	} = props;
+
+	// eslint-disable-next-line no-plusplus
+	for (let index = 0; index < tempArr.length; index++) {
+		if (index === 0) {
+			const currentWeather = setCurrentWeather(index, response, tempArr);
+			addCurntWeather(currentWeather);
+
+			// return {
+			// 	currentWeather,
+			// };
+		} else {
+			const nextWeather = setNearestWeather(index, tempArr, nearestWeather);
+			// console.log(nextWeather);
+			addNearestWeather(nextWeather);
+		}
+	}
+
+	setSearchHistory(response, searchHistory, addSearchHistory);
+	// addSearchHistory(history);
+};
+
+export default setWeatherValue;
