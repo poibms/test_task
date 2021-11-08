db2fcca81af64677778325f730691e3afb01b8a8
Maksim <easypeasylemonskvii@gmail.com>
2021-11-08 14:05:51 +0300

    TT: hoc v2

--- src/Hoc/UserStatus.js
+++ src/Hoc/UserStatus.js
@@ -1,23 +1,46 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { AuthContext } from '../Components/Helpers/AuthContext';
 import { mainPage, signIn } from '../Config/Routes';
 
 function requireAuthentication(Component) {
-	return class Authentication extends React.Component {
+	class Authentication extends React.Component {
 		// eslint-disable-next-line react/static-property-placement
 		static contextType = AuthContext;
 
 		componentDidMount() {
-			if (this.context.isLoggin) {
+			const { user } = this.props;
+			if (user) {
 				this.props.history.push(mainPage);
 			} else {
 				this.props.history.push(signIn);
 			}
 		}
 
+		componentDidUpdate(prevProps) {
+			const { history } = this.props;
+			if (prevProps.user !== this.props.user) {
+				history.push(this.getUserState());
+			}
+		}
+
+		getUserState = () => {
+			const { user } = this.props;
+			if (user) {
+				return mainPage;
+			}
+
+			return signIn;
+		};
+
 		render() {
 			return <Component />;
 		}
-	};
+	}
+
+	return connect((state) => ({
+		user: state.user,
+	}))(Authentication);
 }
+
 export default requireAuthentication;
